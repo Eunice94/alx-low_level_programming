bf4d483c4d22841978aae7fc60697f2a9657e3f2
Eunice <eunicewachira94@gmail.com>
2022-04-08 11:42:19 -0700

    add

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,13 +22,12 @@ int main(void)
 	}
 	else if (n < 0)
 	{
-		printf("%d is negative\n", n)
+		printf("%d is negative\n", n);
 	}
 	else 
 	{
 		printf("%d is not number\n", n);
 	}
 	return (0);
-
 }
 
