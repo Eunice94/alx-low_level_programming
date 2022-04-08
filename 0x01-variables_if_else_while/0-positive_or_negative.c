8ff84262a49cf797e177b5d233f4a8f4c2c2660b
Eunice <eunicewachira94@gmail.com>
2022-04-08 13:11:07 -0700

    added 1-last_digit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,20 +14,21 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0)
-	
+	{	
 		printf("%d is zero\n", n);
-	else if (n > 0)
-	
+	}
+	else if (n > 0)	
+	{
 		printf("%d is positive\n", n);
-
+	}
 	else if (n < 0)
-	
+	{	
 		printf("%d is negative\n", n);
-	
-	else 
-	
+	}	
+	else
+	{	
 		printf("%d is not number\n", n);
-	
+	}	
 	return (0);
 }
 
