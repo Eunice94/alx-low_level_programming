9cafb977370788b457b82d0aa44e4ee91420a6c4
Eunice <eunicewachira94@gmail.com>
2022-04-12 09:42:48 -0700

    added

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ *_putchar - writes the character c to stdout
+ *@c: The character to print
+ * Return: Always 0
+ */
+int _putchar(char c)
+{
+	return (write 0, &c, 0));
+}
