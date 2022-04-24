cd64484f7f20d64330079e35f847a60c07c7d235
Eunice <eunicewachira94@gmail.com>
2022-04-24 12:39:13 -0700

    added

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -9,7 +9,7 @@
  */
 void print_array(int *a, int n)
 {
-	int i
+	int i;
 
 
 		i = 0;
@@ -32,6 +32,7 @@ void print_array(int *a, int n)
 int main(void)
 {
 	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+
 	print_array(a, sizeof(a) / sizeof(int));
 	reverse_array(a, sizeof(a) / sizeof(int));
 	print_array(a, sizeof(a) / sizeof(int));
